8b47bea0f274320a2eb73a127691723569bb3c15
Myllaume <brioudes.guillaume@outlook.fr>
2018-12-30 23:33:35 +0100

    Corrections placement texte

--- index.php
+++ index.php
@@ -50,10 +50,7 @@
             <div class="main--separation"></div>
 
             <section class="main__page col-page">
-                <section class="main__page__code">
-                    <article>
-
-                    </article>
+                <article class="texte-intro-accueil">
                     <p class="lettrine">Déjà avant d'entamer mes études sur le web je développais de petits sites en
                         HTML et CSS. Ces mois de développement en DUT Métiers du Multimédia et de l'Internet m'ont
                         apporté de la technique, de la méthode et la connaissance d'autres langages comme le JavaScript
@@ -63,26 +60,27 @@
                         données, mettre en place une interface pour rendre ces données accessibles aux utilisateurs.</p>
                     <p>L'informatique me donne la possibilité de par le texte de créer ce que je ne sais dessiner,
                         d'afficher de l'information en bonne et due forme à l'internaute.</p>
-
-                    <div class="row">
-                        <a href="./pages/parcours.php" class="main__page__competences--bouton bouton col2">
-                            <div class="bouton-volet-content">
-                                <p>Voir mon parcours</p>
-                                <div class="bouton-volet-animation"></div>
-                            </div>
-                        </a>
-                        <a href="./pages/projet.php" class="main__page__competences--bouton bouton col2">
-                            <div class="bouton-volet-content">
-                                <p>Voir mes projets</p>
-                                <div class="bouton-volet-animation"></div>
-                            </div>
-                        </a>
-                    </div>
-                </section>
-
-                <article class="main__page__num-page">
-                    <p>2</p>
                 </article>
+
+                <div class="row">
+                    <a href="./pages/parcours.php" class="main__page__competences--bouton bouton col2">
+                        <div class="bouton-volet-content">
+                            <p>Voir mon parcours</p>
+                            <div class="bouton-volet-animation"></div>
+                        </div>
+                    </a>
+                    <a href="./pages/projet.php" class="main__page__competences--bouton bouton col2">
+                        <div class="bouton-volet-content">
+                            <p>Voir mes projets</p>
+                            <div class="bouton-volet-animation"></div>
+                        </div>
+                    </a>
+                </div>
+            </section>
+
+            <article class="main__page__num-page">
+                <p>2</p>
+            </article>
             </section>
             <?php include("./include/footer.html"); ?>
         </main>
